447ab67d08e217f1cef8353c46b8c5870c8c8814
Thomas Nelson <thnlsn@gmail.com>
2020-03-18 05:01:28 -0700

    styling for header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import './Header.css';
 
 const Header = () => {
   return (
     <header className='header'>
       <div className='logo-box'>
-        <img src='./images/logo-white.png' alt='' />
+        <img src='./images/logo-white.png' alt='Logo' className='logo' />
       </div>
+      <h1 className='heading-primary'>
+        <span className='heading-primary-main'>Outdoors</span>
+        <span className='heading-primary-sub'>is where life happens</span>
+      </h1>
     </header>
   );
 };
