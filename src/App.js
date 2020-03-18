237a8abd4eec91be45081369141ee5da7f7cf68c
Thomas Nelson <thnlsn@gmail.com>
2020-03-17 21:07:31 -0700

    file structure done (for now), header element set up

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import './App.css';
 
+//Components
+import Header from './components/Header/Header';
+
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Header />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Header = () => {
+  return <header className='header'>Some text</header>;
+};
+
+export default Header;
