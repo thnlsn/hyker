e106335d6833650e02f6c9bf8b95fb9ae61d4810
Thomas Nelson <thnlsn@gmail.com>
2020-03-18 04:26:03 -0700

    header shape done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const Header = () => {
-  return <header className='header'>Some text</header>;
+  return (
+    <header className='header'>
+      <div className='logo-box'>
+        <img src='./images/logo-white.png' alt='' />
+      </div>
+    </header>
+  );
 };
 
 export default Header;
