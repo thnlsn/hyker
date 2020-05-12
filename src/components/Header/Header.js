0bfeb5b277c42569b3af45ccd321562eb5f81a3d
Thomas Nelson <thnlsn@gmail.com>
2020-05-11 23:02:03 -0700

    refactoring to incorporate BEM naming scheme

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,16 +4,20 @@ import './Header.css';
 const Header = () => {
   return (
     <header className='header'>
-      <div className='logo-box'>
-        <img src='./images/logo-white.png' alt='Logo' className='logo' />
+      <div className='header__logo-box'>
+        <img
+          src='./images/logo-white.png'
+          alt='Logo'
+          className='header__logo'
+        />
       </div>
-      <div className='text-box'>
+      <div className='header__text-box'>
         <h1 className='heading-primary'>
-          <span className='heading-primary-main'>Outdoors</span>
-          <span className='heading-primary-sub'>is where life happens</span>
+          <span className='heading-primary--main'>Outdoors</span>
+          <span className='heading-primary--sub'>is where life happens</span>
         </h1>
         {/* Notice the non-usage of the button element */}
-        <a href='#' className='btn btn-white btn-animated'>
+        <a href='#' className='btn btn--white btn--animated'>
           Discover our services
         </a>
       </div>
