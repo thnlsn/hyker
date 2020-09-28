43306ebee4e913dee2010ac17352f5e9242ab261
Thomas Nelson <thnlsn@gmail.com>
2020-09-27 21:09:38 -0700

    working on custom grid columns

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,28 +1,42 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import '../../css/style.css';
 
 // Header
 const Header = () => {
   return (
-    <header className='header'>
-      <div className='header__logo-box'>
-        <img
-          src='./images/logo-white.png'
-          alt='Logo'
-          className='header__logo'
-        />
-      </div>
-      <div className='header__text-box'>
-        <h1 className='heading-primary'>
-          <span className='heading-primary--main'>Outdoors</span>
-          <span className='heading-primary--sub'>is where life happens</span>
-        </h1>
-        {/* Notice the non-usage of the button element, instead using classes to create button effect */}
-        <a href='#' className='btn btn--white btn--animated'>
-          Discover our services
-        </a>
-      </div>
-    </header>
+    <Fragment>
+      <header className='header'>
+        <div className='header__logo-box'>
+          <img
+            src='./images/logo-white.png'
+            alt='Logo'
+            className='header__logo'
+          />
+        </div>
+        <div className='header__text-box'>
+          <h1 className='heading-primary'>
+            <span className='heading-primary--main'>Outdoors</span>
+            <span className='heading-primary--sub'>is where life happens</span>
+          </h1>
+          {/* Notice the non-usage of the button element, instead using classes to create button effect */}
+          <a href='#' className='btn btn--white btn--animated'>
+            Discover our services
+          </a>
+        </div>
+      </header>
+
+      <section className='grid-test'>
+        <div className='row'>
+          <div className='col-1-of-2'>Col 1 of 2</div>
+          <div className='col-1-of-2'>Col 1 of 2</div>
+        </div>
+        <div className='row'>
+          <div className='col-1-of-3'>Col 1 of 3</div>
+          <div className='col-1-of-3'>Col 1 of 3</div>
+          <div className='col-1-of-3'>Col 1 of 3</div>
+        </div>
+      </section>
+    </Fragment>
   );
 };
 
