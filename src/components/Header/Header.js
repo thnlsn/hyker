69b19b1fec837ad2e9450148a3b0b7a01283cb86
Thomas Nelson <thnlsn@gmail.com>
2020-09-29 20:19:43 -0700

    columns and rows structure done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -36,6 +36,25 @@ const Header = () => {
           <div className='col-1-of-3'>Col 1 of 3</div>
           <div className='col-1-of-3'>Col 1 of 3</div>
         </div>
+        <div className='row'>
+          <div className='col-1-of-3'>Col 1 of 3</div>
+          <div className='col-2-of-3'>Col 2 of 3</div>
+        </div>
+        <div className='row'>
+          <div className='col-1-of-4'>Col 1 of 4</div>
+          <div className='col-1-of-4'>Col 1 of 4</div>
+          <div className='col-1-of-4'>Col 1 of 4</div>
+          <div className='col-1-of-4'>Col 1 of 4</div>
+        </div>
+        <div className='row'>
+          <div className='col-1-of-4'>Col 1 of 4</div>
+          <div className='col-1-of-4'>Col 1 of 4</div>
+          <div className='col-12-of-4'>Col 2 of 4</div>
+        </div>
+        <div className='row'>
+          <div className='col-1-of-4'>Col 1 of 4</div>
+          <div className='col-3-of-2'>Col 3 of 2</div>
+        </div>
       </section>
     </Fragment>
   );
