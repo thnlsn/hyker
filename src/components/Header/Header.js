2ac1db1257a6585ffd857e60b58555189a65b0c4
Thomas Nelson <thnlsn@gmail.com>
2020-10-05 13:10:01 -0700

    finished column size 2 out of 4

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -49,16 +49,12 @@ const Header = () => {
         <div className='row'>
           <div className='col-1-of-4'>Col 1 of 4</div>
           <div className='col-1-of-4'>Col 1 of 4</div>
-          <div className='col-12-of-4'>Col 2 of 4</div>
+          <div className='col-2-of-4'>Col 2 of 4</div>
         </div>
       </section>
       <div className='row'>
         <div className='col-1-of-4'>Col 1 of 4</div>
-        <div className='col-3-of-2'>Col 3 of 2</div>
-      </div>
-      <div className='row'>
-        <div className='col-2-of-4'>Col 1 of 4</div>
-        <div className='col-2-of-4'>Col 3 of 2</div>
+        <div className='col-3-of-4'>Col 3 of 4</div>
       </div>
     </Fragment>
   );
