da012815f0fe199467e6864b7ae00293a4df4d73
Thomas Nelson <thnlsn@gmail.com>
2020-10-06 16:50:09 -0700

    created some margin bottom utility classes

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -8,10 +8,15 @@ const About = () => {
       {/* The "main" element is purely semantic, it is used as the wrapper for the sections between the header and footer, or the "main" bulk of your page content */}
       <main>
         <section className='section-about'>
-          <div className='u-center-text'>
+          <div className='u-center-text u-margin-bottom-8'>
             <h2 className='heading-secondary'>
               Exciting hikes for the adventurous!
             </h2>
+          </div>
+
+          <div className='row'>
+            <div className='col-1-of-2'>Text content</div>
+            <div className='col-1-of-2'>Image</div>
           </div>
         </section>
       </main>
