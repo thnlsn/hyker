e266a4f7af5eabcf790405444aac07166b13f853
Thomas Nelson <thnlsn@gmail.com>
2020-10-05 19:37:20 -0700

    went ahead and imported each component into main App component for easy implementation once they are built

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import './css/style.css';
 //Components
 import Header from './components/Header/Header';
 import About from './components/About/About';
+import Features from './components/Features/Features';
+import Booking from './components/Booking/Booking';
+import Hikes from './components/Hikes/Hikes';
+import Stories from './components/Stories/Stories';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
