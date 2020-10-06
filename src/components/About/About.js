5548b1c076aa944dfd039a15c10207e927d99c20
Thomas Nelson <thnlsn@gmail.com>
2020-10-06 09:57:52 -0700

    added a skew and scale effect on hover of the secondary heading, as well as centered it using a newly created utility class

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -8,9 +8,11 @@ const About = () => {
       {/* The "main" element is purely semantic, it is used as the wrapper for the sections between the header and footer, or the "main" bulk of your page content */}
       <main>
         <section className='section-about'>
-          <h2 className='heading-secondary'>
-            Exciting hikes for the adventurous!
-          </h2>
+          <div className='u-center-text'>
+            <h2 className='heading-secondary'>
+              Exciting hikes for the adventurous!
+            </h2>
+          </div>
         </section>
       </main>
     </Fragment>
