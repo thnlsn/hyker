5cc33c7217bab5a7d648bfdc90d756e7840bb6fb
Thomas Nelson <thnlsn@gmail.com>
2020-10-05 19:21:25 -0700

    created component for the about section

---
+++ src/components/About/About.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+import '../../css/style.css';
+
+// About Section
+const About = () => {
+  return (
+    <Fragment>
+      <main></main>
+    </Fragment>
+  );
+};
+
+export default About;
