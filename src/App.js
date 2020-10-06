fd378135adf290e734ec241b66b828dd65925dc5
Thomas Nelson <thnlsn@gmail.com>
2020-10-06 09:25:45 -0700

    created an offwhite background to contrast with the white border of the site, and moved it up with a negative margin so that the white space of the header polygon wouldnt show

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import './css/style.css';
 import Header from './components/Header/Header';
 import About from './components/About/About';
 import Features from './components/Features/Features';
-import Booking from './components/Booking/Booking';
 import Hikes from './components/Hikes/Hikes';
 import Stories from './components/Stories/Stories';
+import Booking from './components/Booking/Booking';
 import Footer from './components/Footer/Footer';
 
 function App() {
