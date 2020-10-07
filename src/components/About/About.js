8800bd98cf84ac31442d648efde4c7f07ed56def
Thomas Nelson <thnlsn@gmail.com>
2020-10-06 17:03:06 -0700

    created styles for tertiary headings and paragraphs that may go with them

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -8,7 +8,7 @@ const About = () => {
       {/* The "main" element is purely semantic, it is used as the wrapper for the sections between the header and footer, or the "main" bulk of your page content */}
       <main>
         <section className='section-about'>
-          <div className='u-center-text u-margin-bottom-8'>
+          <div className='u-center-text u-margin-bottom-large'>
             <h2 className='heading-secondary'>
               Exciting hikes for the adventurous!
             </h2>
