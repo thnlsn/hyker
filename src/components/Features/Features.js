4cc27a63943034511f84c1cbc6e86a84c60f32b3
Thomas Nelson <thnlsn@gmail.com>
2020-10-08 09:38:15 -0700

    removed some unneccesary code

--- src/components/Features/Features.js
+++ src/components/Features/Features.js
@@ -6,20 +6,20 @@ const Features = () => {
   return (
     <Fragment>
       <section className='section-features'>
-        <row className=''>
+        <div className='row'>
           <div className='col-1-of-4'>
             <div className='feature-box'>
+              {/* Create a box within the grid because we want to edit some spacing and add scaling, but don't want to directly edit the grid code */}
               <i className='feature-box__icon icon-basic-world'></i>
               <h3 className='heading-tertiary'>Explore the world</h3>
               <p className='feature-box__text'>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 Dolorem, molestias? Officiis iusto atque dolore deleniti
-                repellat blanditiis perspiciatis omnis quos! Quibusdam autem
-                obcaecati quia nisi labore optio facilis expedita ut?
+                repellat blanditiis perspiciatis omnis quos!
               </p>
             </div>
           </div>
-        </row>
+        </div>
       </section>
     </Fragment>
   );
