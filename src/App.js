1e93edf4fab8fc2031f4d1d7848277daf691fbdd
Thomas Nelson <thnlsn@gmail.com>
2020-10-08 11:42:52 -0700

    removed unneccesary height declaration

--- src/App.js
+++ src/App.js
@@ -19,6 +19,10 @@ function App() {
       <main>
         <About />
         <Features />
+        <Hikes />
+        <Stories />
+        <Booking />
+        <Footer />
       </main>
     </div>
   );
