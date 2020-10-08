7bbc4925b199a32c35750402700a75e308ac4b24
Thomas Nelson <thnlsn@gmail.com>
2020-10-08 14:57:45 -0700

    fixed positioning for cards

--- src/components/Hikes/Hikes.js
+++ src/components/Hikes/Hikes.js
@@ -5,14 +5,29 @@ import '../../css/style.css';
 const Hikes = () => {
   return (
     <Fragment>
-      <section className='section-tours'>
+      <section className='section-hikes'>
         <div className='u-center-text u-margin-bottom-big'>
           <h2 className='heading-secondary'>Most popular hikes</h2>
         </div>
         <div className='row'>
-          <div className='col-1-of-3'></div>
-          <div className='col-1-of-3'></div>
-          <div className='col-1-of-3'></div>
+          <div className='col-1-of-3'>
+            <div className='card'>
+              <div className='card__side card__side--front'>TEXT</div>
+              <div className='card__side card__side--back'>TEXT</div>
+            </div>
+          </div>
+          <div className='col-1-of-3'>
+            <div className='card'>
+              <div className='card__side card__side--front'>TEXT</div>
+              <div className='card__side card__side--back'>TEXT</div>
+            </div>
+          </div>
+          <div className='col-1-of-3'>
+            <div className='card'>
+              <div className='card__side card__side--front'>TEXT</div>
+              <div className='card__side card__side--back'>TEXT</div>
+            </div>
+          </div>
         </div>
       </section>
     </Fragment>
