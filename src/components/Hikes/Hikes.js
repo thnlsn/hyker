44838a93ffb661aa6b08f342701b6ce8da6dff34
Thomas Nelson <thnlsn@gmail.com>
2020-10-08 11:52:49 -0700

    edited transition speed on secondary headings

--- src/components/Hikes/Hikes.js
+++ src/components/Hikes/Hikes.js
@@ -3,7 +3,20 @@ import '../../css/style.css';
 
 // Hikes Section
 const Hikes = () => {
-  return <Fragment></Fragment>;
+  return (
+    <Fragment>
+      <section className='section-tours'>
+        <div className='u-center-text u-margin-bottom-big'>
+          <h2 className='heading-secondary'>Most popular hikes</h2>
+        </div>
+        <div className='row'>
+          <div className='col-1-of-3'></div>
+          <div className='col-1-of-3'></div>
+          <div className='col-1-of-3'></div>
+        </div>
+      </section>
+    </Fragment>
+  );
 };
 
 export default Hikes;
