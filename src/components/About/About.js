3b121bc616638ecf21e802d5cc46675a30365f15
Thomas Nelson <thnlsn@gmail.com>
2020-10-07 17:34:41 -0700

    created html skeleton for the features section and an scss component for each feature box

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -65,6 +65,23 @@ const About = () => {
             </div>
           </div>
         </section>
+
+        <section className='section-features'>
+          <row className=''>
+            <div className='col-1-of-4'>
+              <div className='feature-box'>
+                <i className='feature-box__icon icon-basic-world'></i>
+                <h3 className='heading-tertiary'>Explore the world</h3>
+                <p className='feature-box__text'>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Dolorem, molestias? Officiis iusto atque dolore deleniti
+                  repellat blanditiis perspiciatis omnis quos! Quibusdam autem
+                  obcaecati quia nisi labore optio facilis expedita ut?
+                </p>
+              </div>
+            </div>
+          </row>
+        </section>
       </main>
     </Fragment>
   );
