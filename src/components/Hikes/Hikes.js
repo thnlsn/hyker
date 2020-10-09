aa839cfcc8f52e1b083992a8130258542c3dacf4
Thomas Nelson <thnlsn@gmail.com>
2020-10-09 12:02:49 -0700

    applied a clone box decoration break so that padding applies to any boxes created by text breaking in smaller viewports to help with responsiveness

--- src/components/Hikes/Hikes.js
+++ src/components/Hikes/Hikes.js
@@ -16,7 +16,11 @@ const Hikes = () => {
             <div className='card'>
               <div className='card__side card__side--front'>
                 <div className='card__picture card__picture--1'>&nbsp;</div>
-                <div className='card__heading'>The Oceanside Explorer</div>
+                <h4 className='card__heading'>
+                  <span className='card__heading-span card__heading-span--1'>
+                    The Ocean Explorer
+                  </span>
+                </h4>
                 <div className='card__details'>Details</div>
               </div>
               <div className='card__side card__side--back card__side--back-1'>
@@ -27,9 +31,12 @@ const Hikes = () => {
           <div className='col-1-of-3'>
             <div className='card'>
               <div className='card__side card__side--front'>
-                {' '}
                 <div className='card__picture card__picture--2'>&nbsp;</div>
-                <div className='card__heading'>The Deep Forest Adventurer</div>
+                <h4 className='card__heading'>
+                  <span className='card__heading-span card__heading-span--2'>
+                    The Forest Adventurer
+                  </span>
+                </h4>
                 <div className='card__details'>Details</div>
               </div>
               <div className='card__side card__side--back card__side--back-2'>
@@ -40,9 +47,12 @@ const Hikes = () => {
           <div className='col-1-of-3'>
             <div className='card'>
               <div className='card__side card__side--front'>
-                {' '}
                 <div className='card__picture card__picture--3'>&nbsp;</div>
-                <h4 className='card__heading'>The Tundra Bound Traveler</h4>
+                <h4 className='card__heading'>
+                  <span className='card__heading-span card__heading-span--3'>
+                    The Tundra Traveller
+                  </span>
+                </h4>
                 <div className='card__details'>Details</div>
               </div>
               <div className='card__side card__side--back card__side--back-3'>
