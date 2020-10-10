6eb74982bec4fa2434d37ad41c87a92e36874eda
Thomas Nelson <thnlsn@gmail.com>
2020-10-09 18:29:27 -0700

    created classes and basic positioning styling for the back side of the card components

--- src/components/Hikes/Card/CardBack.js
+++ src/components/Hikes/Card/CardBack.js
@@ -3,11 +3,15 @@ import React from 'react';
 const CardBack = ({ num, price }) => {
   return (
     <div className={`card__side card__side--back card__side--back-${num}`}>
-      <div>only</div>
-      <div>{price}</div>
-      <a href='#' className='btn btn--white'>
-        Book now!
-      </a>
+      <div className='card__cta'>
+        <div className='card__price-box'>
+          <p className='card__price-only'>Only</p>
+          <p className='card__price-value'>{price}</p>
+        </div>
+        <a href='#' className='btn btn--white'>
+          Book now!
+        </a>
+      </div>
     </div>
   );
 };
