1baa681a5ccdea45d4c9e8bae97f4e20dc601163
Thomas Nelson <thnlsn@gmail.com>
2020-10-10 20:06:40 -0700

    booking form skeleton build and sass file imported into the main sass file

--- src/components/Booking/Booking.js
+++ src/components/Booking/Booking.js
@@ -7,7 +7,40 @@ const Booking = () => {
     <Fragment>
       <section className='section-booking'>
         <div className='row'>
-          <div className='booking'></div>
+          <div className='booking'>
+            <div className='booking__form'>
+              <div className='u-margin-bottom-medium'>
+                <h2 className='heading-secondary'>Start booking now</h2>
+              </div>
+              <form action='#' className='form'>
+                <div className='form__group'>
+                  <input
+                    type='text'
+                    className='form__input'
+                    placeholder='Full name'
+                    id='name'
+                    required
+                  />
+                  <label htmlFor='name' className='form__label'>
+                    Full name
+                  </label>
+                </div>
+
+                <div className='form__group'>
+                  <input
+                    type='text'
+                    className='form__input'
+                    placeholder='Email address'
+                    id='email'
+                    required
+                  />
+                  <label htmlFor='name' className='form__label'>
+                    Email address
+                  </label>
+                </div>
+              </form>
+            </div>
+          </div>
         </div>
       </section>
     </Fragment>
