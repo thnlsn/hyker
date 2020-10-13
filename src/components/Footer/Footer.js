e157af7060e620aa3e9cef69891109c9f451cb6d
Thomas Nelson <thnlsn@gmail.com>
2020-10-12 18:45:43 -0700

    created markup skeleton of the footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,21 @@
 import React, { Fragment } from 'react';
 import '../../css/style.css';
 
+const logo = require('../../images/logo-green-2x.png');
+
 // Footer Section
 const Footer = () => {
   return (
     <Fragment>
-      <footer className='footer'>footer</footer>
+      <footer className='footer'>
+        <div className='footer__logo-box'>
+          <img src={logo} alt='Full Logo' />
+        </div>
+        <div className='row'>
+          <div className='col-1-of-2'></div>
+          <div className='col-1-of-2'></div>
+        </div>
+      </footer>
     </Fragment>
   );
 };
