a9126d8627f1168f980d6c696ff6ede80c2092cd
Thomas Nelson <thnlsn@gmail.com>
2020-10-13 09:37:00 -0700

    created skeleton for the footer links and text

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -12,8 +12,38 @@ const Footer = () => {
           <img src={logo} alt='Full Logo' />
         </div>
         <div className='row'>
-          <div className='col-1-of-2'></div>
-          <div className='col-1-of-2'></div>
+          <div className='col-1-of-2'>
+            <ul className='footer__list'>
+              <li className='footer__item'>
+                <a href='#' className='footer__link'>
+                  Company
+                </a>
+                <a href='#' className='footer__link'>
+                  Contact us
+                </a>
+                <a href='#' className='footer__link'>
+                  Careers
+                </a>
+                <a href='#' className='footer__link'>
+                  Privacy
+                </a>
+                <a href='#' className='footer__link'>
+                  Terms
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className='col-1-of-2'>
+            <p className='footer__copyright'>
+              Built by{' '}
+              <a href='#' className='footer__link'>
+                {' '}
+                Thomas Nelson
+              </a>{' '}
+              as practice learning CSS and Sass.
+            </p>
+            Copyright &copy; by Jonas Schmedtmann
+          </div>
         </div>
       </footer>
     </Fragment>
