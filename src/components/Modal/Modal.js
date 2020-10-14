572ff936cdb54046c7de9a585f271f6a4b330a3b
Thomas Nelson <thnlsn@gmail.com>
2020-10-14 14:09:26 -0700

    created full skeleton of the modal content box

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,9 +1,44 @@
 import React from 'react';
 
+const tourPhoto1 = require('../../images/nat-8.jpg');
+const tourPhoto2 = require('../../images/nat-9.jpg');
+
 const Modal = () => {
   return (
     <div className='modal'>
-      <div className='modal__content'></div>
+      <div className='modal__content'>
+        <div className='modal__left'>
+          <img src={tourPhoto1} alt='Tour photo' className='modal__img' />
+          <img src={tourPhoto2} alt='Tour photo' className='modal__img' />
+        </div>
+        <div className='modal__right'>
+          <h2 className='heading-secondary'>Start booking now</h2>
+          <h3 className='heading-tertiary'>
+            Important &ndash; Please read carefully before booking
+          </h3>
+          <p className='modal__text'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Aliquet
+            bibendum enim facilisis gravida neque convallis a cras. Odio ut sem
+            nulla pharetra diam. Tincidunt dui ut ornare lectus sit. Posuere
+            urna nec tincidunt praesent semper feugiat nibh sed. Molestie ac
+            feugiat sed lectus. Lectus mauris ultrices eros in. Suspendisse
+            faucibus interdum posuere lorem ipsum dolor sit. Semper risus in
+            hendrerit gravida rutrum. Vel eros donec ac odio tempor. In metus
+            vulputate eu scelerisque felis imperdiet proin fermentum. Tellus
+            integer feugiat scelerisque varius morbi enim. Maecenas sed enim ut
+            sem viverra aliquet. Venenatis cras sed felis eget velit aliquet
+            sagittis id consectetur. Eget nunc scelerisque viverra mauris in
+            aliquam sem fringilla ut. Rhoncus mattis rhoncus urna neque. Rhoncus
+            mattis rhoncus urna neque viverra justo nec ultrices. Netus et
+            malesuada fames ac turpis egestas maecenas pharetra. Enim eu turpis
+            egestas pretium aenean pharetra magna ac placerat.
+          </p>
+          <a href='#' className='btn btn--green'>
+            Book now
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
