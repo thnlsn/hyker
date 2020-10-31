f6e52547afd0535ba3a45c989276941f14610a49
Thomas Nelson <thnlsn@gmail.com>
2020-10-30 23:48:43 -0700

    made the hike cards display using a map function

--- src/components/Hikes/Hikes.js
+++ src/components/Hikes/Hikes.js
@@ -3,6 +3,30 @@ import '../../css/style.css';
 
 import Card from './Card/Card';
 
+const cards = [
+  {
+    href: '#modal',
+    num: '1',
+    heading: 'The Ocean Explorer',
+    details: [3, 30, 2, 'cozy hotels', 'easy'],
+    price: '297',
+  },
+  {
+    href: '#modal',
+    num: '2',
+    heading: 'The Forest Adventurer',
+    details: [7, 40, 6, 'provided tents', 'medium'],
+    price: '497',
+  },
+  {
+    href: '#modal',
+    num: '3',
+    heading: 'The Tundra Traveller',
+    details: [5, 15, 3, 'provided tents', 'hard'],
+    price: '897',
+  },
+];
+
 // Hikes Section
 const Hikes = () => {
   return (
@@ -15,33 +39,17 @@ const Hikes = () => {
         {/* Add a way to pass in background images, in the future */}
 
         <div className='row'>
-          <div className='col-1-of-3'>
-            <Card
-              href='#modal'
-              num='1'
-              heading='The Ocean Explorer'
-              details={[3, 30, 2, 'cozy hotels', 'easy']}
-              price='297'
-            />
-          </div>
-          <div className='col-1-of-3'>
-            <Card
-              href='#modal'
-              num='2'
-              heading='The Forest Adventurer'
-              details={[7, 40, 6, 'provided tents', 'medium']}
-              price='497'
-            />
-          </div>
-          <div className='col-1-of-3'>
-            <Card
-              href='#modal'
-              num='3'
-              heading='The Tundra Traveller'
-              details={[5, 15, 3, 'provided tents', 'hard']}
-              price='897'
-            />
-          </div>
+          {cards.map((card) => (
+            <div className='col-1-of-3'>
+              <Card
+                href={card.href}
+                num={card.num}
+                heading={card.heading}
+                details={card.details}
+                price={card.price}
+              />
+            </div>
+          ))}
         </div>
 
         <div className='u-center-text u-margin-top-xl'>
